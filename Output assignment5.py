6cd31f2531d8d112ace40565b88f0e43b4958351
Shreyash2003 <104262663+Shreyash2003@users.noreply.github.com>
2022-06-15 22:57:28 +0530

    Add files via upload

---
+++ Output assignment5.py
@@ -0,0 +1,130 @@
+C:\Users\shrey\PycharmProjects\pythonProject42\venv\Scripts\python.exe C:/Users/shrey/PycharmProjects/pythonProject42/main.py
+Q1
+Enter a string(without apostrophes) and it will be reversed:Python is Love
+Reversed string: evoL si nohtyP
+
+Q2
+Enter starting range:12
+Enter ending range:100
+Enter a number for which the entered range will be divisible by:4
+12
+16
+20
+24
+28
+32
+36
+40
+44
+48
+52
+56
+60
+64
+68
+72
+76
+80
+84
+88
+92
+96
+100
+
+Q3
+Enter length of first side of the triangle:8
+Enter length of second side of the triangle:7
+Enter length of third side of the triangle:3
+Area of the trianle is 10.392304845413264
+
+Q4
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+
+Q5
+Enter number of rows:7
+A
+B C
+D E F
+G H I J
+K L M N O
+P Q R S T U
+V W X Y Z [ \
+
+Q6
+Prime numbers in an input range.
+Enter starting of range(>=2):35
+Enter ending of range:120
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+83
+89
+97
+101
+103
+107
+109
+113
+
+Q7
+77
+154
+231
+308
+385
+462
+
+Q8
+Enter integer:45
+Enter integer:23
+Enter integer:13
+Enter integer:78
+Enter integer:96
+Enter integer:81
+Enter integer:68
+Enter integer:70
+Enter integer:99
+a) Positive integers: [45, 23, 13, 78, 96, 81, 68, 70, 99]
+b) Negative integers: []
+c) Odd integers: [45, 23, 13, 81, 99]
+d) Even integers: [78, 96, 68, 70]
+e)
+integer:45 ; number of times it occurs: 1
+integer:23 ; number of times it occurs: 1
+integer:13 ; number of times it occurs: 1
+integer:78 ; number of times it occurs: 1
+integer:96 ; number of times it occurs: 1
+integer:81 ; number of times it occurs: 1
+integer:68 ; number of times it occurs: 1
+integer:70 ; number of times it occurs: 1
+integer:99 ; number of times it occurs: 1
+
+Q9
+Enter a list of words(strings):['shreyash','shreyash','vaibhav','gursimar','amisha','vaibhav','kundu','kundu','dheeraj','dheeraj','vishwas','harsh','vishwas','samridhi']
+word:shreyash ; number of times it occurs:2
+word:vaibhav ; number of times it occurs:2
+word:gursimar ; number of times it occurs:1
+word:amisha ; number of times it occurs:1
+word:kundu ; number of times it occurs:2
+word:dheeraj ; number of times it occurs:2
+word:vishwas ; number of times it occurs:2
+word:harsh ; number of times it occurs:1
+word:samridhi ; number of times it occurs:1
+
+Process finished with exit code 0
